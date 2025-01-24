b3734c0a02c23d66a5ed0f7bace40140f71cbe6d
zaid9528 <mdzaid9528@gmail.com>
2025-01-25 01:27:07 +0530

    add comment in this js file

--- pr.js
+++ pr.js
@@ -1,4 +1,4 @@
- let a=document.getElementById('btn');
+ let a=document.getElementById('btn');//I create this separate file to show the use of event listener and how to use
  a.addEventListener('click',function(){
      alert('this is a button');
  });
